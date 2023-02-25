13bbdce8959675e2659e6dbcff326e5117bf38cd
Nick <example@gmail.com>
2023-02-25 08:38:04 +0200

    new version

---
+++ jsfolder/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let user = [];
+
+user [0] = prompt('Какое ваше имя');
+user [1] = prompt('Введите число');
+user [2] = prompt('Приветствую на своем репозитории');
+user [3] = prompt('Сколько вам лет?');
+
+console.log(user);
+
+let question = confirm('Вас зовут Николай, я угадал?');
+
+let user = 'Привет';
+let  players = ' Как дела?';
+console.log(`user ${players}`);
